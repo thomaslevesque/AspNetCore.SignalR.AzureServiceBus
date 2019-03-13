83b78e0bcb5b08f5f76f5f516ade34f756dc1a53
Thomas Levesque <thomaslevesque@users.noreply.github.com>
2019-03-13 02:21:15 +0100

    Add unit tests for recipients

---
+++ tests/AspNetCore.SignalR.AzureServiceBus.UnitTests/Recipients/ClientsMessageRecipientTests.cs
@@ -0,0 +1,21 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using AspNetCore.SignalR.AzureServiceBus.Internal.Recipients;
+using Xunit;
+
+namespace AspNetCore.SignalR.AzureServiceBus.UnitTests.Recipients
+{
+    public class ClientsMessageRecipientTests
+    {
+        [Fact]
+        public async Task SendCoreAsync_Sends_Message_To_Specified_Clients()
+        {
+            var connectionIds = new[] { "abcd", "efgh" };
+
+            await RecipientTestHelper.AssertSendAsync(
+                new ClientsMessageRecipient { ConnectionIds = connectionIds },
+                (sender, methodName, args) =>
+                    () => sender.SendConnectionsAsync(connectionIds, methodName, args, CancellationToken.None));
+        }
+    }
+}
---
+++ tests/AspNetCore.SignalR.AzureServiceBus.UnitTests/Recipients/UserMessageRecipientTests.cs
@@ -0,0 +1,21 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using AspNetCore.SignalR.AzureServiceBus.Internal.Recipients;
+using Xunit;
+
+namespace AspNetCore.SignalR.AzureServiceBus.UnitTests.Recipients
+{
+    public class UserMessageRecipientTests
+    {
+        [Fact]
+        public async Task SendCoreAsync_Sends_Message_To_Specified_User()
+        {
+            string userId = "abcd";
+
+            await RecipientTestHelper.AssertSendAsync(
+                new UserMessageRecipient { UserId = userId },
+                (sender, methodName, args) =>
+                    () => sender.SendUserAsync(userId, methodName, args, CancellationToken.None));
+        }
+    }
+}
